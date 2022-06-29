cfb67539437bf73e3a88311b47a89cd06c3342d4
rose-ele369 <rosa-234-1@hotmail.com>
2022-06-29 12:46:05 +0200

    Al programa, ya se le puede añadir una lista de tareas.

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -1,18 +1,37 @@
 import React, { useState } from "react";
 import TareaFormulario from "./TareaFormulario";
+import Tarea from "./Tarea";
 import "../estilos/ListaDeTareas.css"
 
 function ListaDeTareas(){
 
 const [tareas, setTareas] = useState([]);
 
+const agregarTarea = tarea => {
+  if (tarea.texto.trim()){
+    tarea.texto=tarea.texto.trim();
+    const tareaActualizadas = [tarea, ...tareas];
+    setTareas(tareaActualizadas);
+  }
+  
+}
+
     return (
         /*Utilizar etiquetas vacías se llaman fragmentos y sirven para poder escribir la estrucutra*/
         <>
-          <TareaFormulario />
+          <TareaFormulario onSubmit={agregarTarea} />
           <div className="tareas-listas-contenedor">
-            Lista de Tareas
+            {
+              tareas.map((tarea) =>
+                <Tarea
+                  key={tarea.id}
+                  id={tarea.id}
+                  texto={tarea.texto}
+                  completada={tarea.completada}
+                  />
 
+              )
+            }
           </div>
         </>
     );
